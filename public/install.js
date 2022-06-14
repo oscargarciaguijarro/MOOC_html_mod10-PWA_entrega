8622def147c39e29630bc8048852854446060ec2
oscargarciaguijarro <107467413+oscargarciaguijarro@users.noreply.github.com>
2022-06-14 11:51:54 +0200

    Add files via upload

---
+++ public/install.js
@@ -0,0 +1,31 @@
+
+'use strict';
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+function saveBeforeInstallPromptEvent(evt) {
+     deferredInstallPrompt = evt;
+     installButton.removeAttribute('hidden');
+     }
+     
+     function installPWA(evt) {
+        deferredInstallPrompt.prompt();
+        evt.srcElement.setAttribute('hidden', true);
+        deferredInstallPrompt.userChoice
+        .then((choice) => {
+            if (choice.outcome === 'accepted') {
+                 console.log('Aceptado', choice);
+            } else {
+                 console.log('No aceptado', choice);
+            }
+            deferredInstallPrompt = null;
+        });
+     }
+     
+window.addEventListener('appinstalled', logAppInstalled);
+
+function logAppInstalled(evt){
+     console.log('App instalada');
+}
+
